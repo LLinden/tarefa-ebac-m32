7eacdecf8f9e1614effc3e7145c4e59828d3952b
Lucas Di Franco Linden <lucas.linden@cwi.com.br>
2023-08-19 10:04:55 -0300

    first commit

--- exercicio1.sql
+++ exercicio1.sql
@@ -7,3 +7,14 @@ ON fc.film_id = f.film_id
 INNER JOIN category c
 ON c.category_id = fc.category_id
 ORDER BY f.title ASC
+
+-- Exercício 2
+-- Faça uma consulta que retorne a lista de todos os atores com o número filmes que cada ator participou. Ordene a lista pelo numero de filmes, 
+-- iniciando pelos atores que mais atuaram
+SELECT a.actor_id, a.first_name, a.last_name, COUNT(fa.film_id) AS numeroDefilmes
+FROM actor a
+INNER JOIN film_actor fa 
+ON a.actor_id = fa.actor_id
+GROUP BY a.actor_id, a.first_name, a.last_name
+ORDER BY numeroDefilmes DESC;
+
