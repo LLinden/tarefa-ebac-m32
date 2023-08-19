61e5ff10760ea4c31ff2fc3efd751c39367b39f2
Lucas Di Franco Linden <lucas.linden@cwi.com.br>
2023-08-19 10:16:18 -0300

    first commit

--- exercicio1.sql
+++ exercicio1.sql
@@ -18,3 +18,13 @@ ON a.actor_id = fa.actor_id
 GROUP BY a.actor_id, a.first_name, a.last_name
 ORDER BY numeroDefilmes DESC;
 
+-- Exercício 3
+-- Faça uma consulta que retorne a lista de atores que atuaram em filmes com mais de duas horas de
+-- duração (120min). Ordene a lista pelo numero de filmes que cada ator participou.
+SELECT a.actor_id, a.first_name, a.last_name, COUNT(f.film_id) AS numero_filmes
+FROM actor a
+INNER JOIN film_actor fa ON a.actor_id = fa.actor_id
+INNER JOIN film f ON fa.film_id = f.film_id
+WHERE f.length > 120
+GROUP BY a.actor_id, a.first_name, a.last_name
+ORDER BY numero_filmes DESC;
